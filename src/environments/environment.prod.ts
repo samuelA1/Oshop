2850dfb04cbdd8bf2a7008e3cbcd2ffdc8bf919b
Samuel Arrey Essim <arreyessimbate@hotmail.com>
2018-02-05 01:43:41 -0500

    Database setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyD5__EbB306QTjG5FD_qosCY9eQ-L0m4rM",
+    authDomain: "oshop-fc9c5.firebaseapp.com",
+    databaseURL: "https://oshop-fc9c5.firebaseio.com",
+    projectId: "oshop-fc9c5",
+    storageBucket: "oshop-fc9c5.appspot.com",
+    messagingSenderId: "65952553561"
+  }
+};
