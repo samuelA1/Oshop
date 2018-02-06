09bad9524c952450d180415a6e80f48fa3f9e4d7
Samuel Arrey Essim <arreyessimbate@hotmail.com>
2018-02-05 21:08:39 -0500

    implement authentication

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent  {
+  user: firebase.User;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe(user => this.user = user);
   }
+
+  logout() {
+    this.afAuth.auth.signOut()
+  }
+
 
 }
